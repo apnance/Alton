6ea8466c02f6fd028aaf6f18bd8c0a1aa02035a9
Aaron Nance <apnance@hotmail.com>
2024-04-01 13:31:15 -0700

    Update

--- AltonTests/AltonTests.swift
+++ AltonTests/AltonTests.swift
@@ -10,8 +10,7 @@ import XCTest
 
 final class AltonTests: XCTestCase {
     
-    func testSolver1() {
-        
+    func testSolver() {
         
         let testOperands =  [
             
@@ -32,6 +31,11 @@ final class AltonTests: XCTestCase {
             
         }
         
+    }
+    
+    func testMyPatience() {
+        let op = [3,5,7,9]
+        Solver(op[0],op[1],op[2],op[3]).echoResults(op)
     }
     
 }
