df5b3d0745b720ca5c480855fd5f2ea2c2d24931
Aaron Nance <apnance@hotmail.com>
2025-09-23 14:39:48 -0700

    Update

---
+++ Alton/TODO.swift
@@ -0,0 +1,18 @@
+//
+//  TODO.swift
+//  Alton
+//
+//  Created by Aaron Nance on 9/19/25.
+//
+
+//Here...
+// 1. Commit/Push code updates.
+// Migration Notes See: /Users/aaron/Documents/Xcode/ConsoleViewClientMigrationMap-v1.6.12.txt
+// 2. DO WORDLER NEXT - IT HAS ALREADY HAD IT'S CONSOLEVIEW PACKAGE UPDATED AND IS BROKEN ATM...
+
+/*---------------------BEG : OLD---------------------*/
+// [09/23/25] committed/pushed updates
+// [09/23/25] Added support for resizing ConsoleView's screen to accomodate the keyboard accessory.
+// [09/22/25] Finished adding unit tests for all commands.
+// [09/22/25] Finished updating all commands.
+/*---------------------END : OLD---------------------*/
