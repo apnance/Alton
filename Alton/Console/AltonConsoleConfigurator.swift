c90d3a9392a74610e8e38072544956baeb421a06
Aaron Nance <apnance@hotmail.com>
2025-09-24 16:20:58 -0700

    Tweak: CVA Keyboard colors via new ConsoleViewConfigs settings

--- Alton/Console/AltonConsoleConfigurator.swift
+++ Alton/Console/AltonConsoleConfigurator.swift
@@ -55,9 +55,30 @@ struct AltonConsoleConfigurator: ConsoleConfigurator {
         configs.shouldMakeCommandFirstResponder     = true
         configs.shouldHideOnScreenTap               = true
         
-        configs.fgColorHistoryBarCommand            = .systemOrange
-        configs.fgColorHistoryBarCommandArgument    = .systemYellow
-        configs.bgColorHistoryBarMain               = .black.pointEightAlpha
+        
+        let blue1    = UIColor.systemBlue
+        let orange1 = UIColor.systemOrange
+        let orange2 = UIColor.systemOrange.pointSevenAlpha
+        let yellow1 = UIColor.systemYellow
+        let yellow2 = UIColor.systemYellow.pointSevenAlpha
+        let white1  = UIColor.white
+        let white2  = UIColor.white.pointEightAlpha
+        
+        let black1  = UIColor.black.pointNineAlpha
+        let black2  = UIColor.black.pointSevenAlpha
+        
+        // Keyboard Accessory View Colors
+        configs.cvaBGColor                          = black2
+        configs.cvaHistoryBar                       = ColorPalette(bg: black1,       fg: blue1, brdr: blue1)
+        configs.cvaCommandToken                     = ColorPalette(bg: black1,       fg: orange1, brdr: orange1)
+        configs.cvaCommandRaw                       = ColorPalette(bg: black1,       fg: orange2, brdr: orange2)
+        configs.cvaCommandFlag                      = ColorPalette(bg: black1,       fg: white2, brdr: white2)
+        configs.cvaNumeric                          = ColorPalette(bg: black1,       fg: yellow1, brdr: yellow1)
+        configs.cvaOperator                         = ColorPalette(bg: black1,       fg: white1, brdr: white2)
+        configs.cvaQuotes                           = ColorPalette(bg: black1,       fg: orange2, brdr: orange2)
+        configs.cvaPound                            = ColorPalette(bg: black1,       fg: orange2, brdr: orange2)
+        configs.cvaParenthetical                    = ColorPalette(bg: black1,       fg: orange2, brdr: orange2)
+        configs.cvaUndefined                        = ColorPalette(bg: .systemGray2, fg: white2, brdr: white2)
         
         configs.shouldShowOnLoad                    = false
         
